852a2b9e60e0eb8171dac8911b68c3d0b39a13d7
nthang2 <nguyenxthang7@gmail.com>
2023-05-29 17:04:15 +0700

    figure collection

--- src/main/java/hust/soict/oop/scraper/datacollection/FigureDataCollector.java
+++ src/main/java/hust/soict/oop/scraper/datacollection/FigureDataCollector.java
@@ -1,5 +1,50 @@
-package hust.soict.oop.scraper.datacollection;
 
-public class FigureDataCollector {
+package datacollection;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+
+
+
+public abstract class FigureDataCollector {
+	//constructor
+	public FigureDataCollector() {}
+
+	//check connection
+	public boolean Connect(String url) {
+		try {
+			Connection.Response response = Jsoup.connect(url).execute();
+			int statusCode = response.statusCode();
+			
+			if (statusCode == 200) {
+				return true;
+			}
+			
+		}
+		
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public Document getDoc(String url) throws IOException {	
+		Connection con = Jsoup.connect(url);
+		Document doc = con.get();
+		return doc;	
+	}
+	
+
+	
+	public abstract  List<String> getAllUrls (String url) throws IOException;
+	public abstract void getData (List<String> allUrls) throws IOException;
+	public abstract void Start() throws IOException;
+
+
+	
 }
